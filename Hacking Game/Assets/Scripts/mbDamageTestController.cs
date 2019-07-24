25b37b9b9494638c1ee06d918defd03f2000bb8b
Steven Serra Mock <GomorrahDotD@users.noreply.github.com>
2019-07-24 19:06:28 +0200

    TestMap Added
    The TestMap Scene's intention is to test things out such as damage, behaviours of objects and every other mechanic

---
+++ Hacking Game/Assets/Scripts/mbDamageTestController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mbDamageTestController : MonoBehaviour
+{
+    private mbEnemy enemyScript;
+
+
+    private void Start()
+    {
+        enemyScript = FindObjectOfType(typeof(mbEnemy)) as mbEnemy;
+    }
+    public void OnButtonDoDamage()
+    {
+        
+        enemyScript.OnButtonDoDamage();
+    }
+}
+
---
+++ Hacking Game/Assets/Scripts/mbEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mbEnemy : MonoBehaviour
+{
+    #region
+    [Header("Leben")]
+
+    [SerializeField]
+    private float startHealth;
+    private float health;
+
+    public Image healthBar;
+    #endregion
+
+    private void Start()
+    {
+        health = startHealth;        
+    }
+
+    public void OnButtonDoDamage()
+    {
+        InflictDamage();
+    }
+
+    private void InflictDamage()
+    {
+        Debug.Log("Damage Taken");
+
+        health -= startHealth * 0.1f;
+        healthBar.fillAmount = health / startHealth;
+
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
